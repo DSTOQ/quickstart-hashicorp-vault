959e6936aa7f0e0c988b548f33e672b987505e6b
Yuriy Lazarev <Unisay@users.noreply.github.com>
2018-10-24 10:59:06 +0200

    deployable to eu-central-1

--- deploy-s3.sh
+++ deploy-s3.sh
@@ -4,6 +4,7 @@ set -e
 
 aws s3 sync . s3://quickstart-hashicorp-vault \
 	--delete \
+	--acl="public-read" \
 	--exclude="*" \
 	--include="scripts/*" \
 	--include="templates/*" \
@@ -14,3 +15,10 @@ aws s3 sync . s3://quickstart-hashicorp-vault \
 	--include="submodules/quickstart-linux-bastion/scripts/*" 
 
 
+aws cloudformation create-stack \
+    --stack-name "DSTOQ-Vault" \
+	--template-url "http://quickstart-hashicorp-vault.s3-eu-central-1.amazonaws.com/templates/quickstart-hashicorp-vault-master.template" \
+	--parameters "ParameterKey=KeyPairName,ParameterValue=DSTOQ_KP_1" \
+	--timeout-in-minutes 30 \
+	--capabilities CAPABILITY_IAM \
+	
