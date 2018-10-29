485b2e4d61eb4d089ffeeda2e6e18890c3d7c135
dmytr <dmytr@users.noreply.github.com>
2018-10-29 17:58:33 +0100

    Allow access from peered VPC

--- deploy-s3.sh
+++ deploy-s3.sh
@@ -12,13 +12,25 @@ aws s3 sync . s3://quickstart-hashicorp-vault \
 	--include="submodules/quickstart-hashicorp-consul/templates/*" \
 	--include="submodules/quickstart-hashicorp-consul/scripts/*" \
 	--include="submodules/quickstart-linux-bastion/templates/*" \
-	--include="submodules/quickstart-linux-bastion/scripts/*" 
+	--include="submodules/quickstart-linux-bastion/scripts/*"
 
+if [ "$1" == "create" ]; then
 
-aws cloudformation create-stack \
-    --stack-name "Vault-Staging" \
+    aws cloudformation create-stack \
+        --stack-name "Vault-Staging" \
 	--template-url "http://quickstart-hashicorp-vault.s3.amazonaws.com/templates/quickstart-hashicorp-vault-master.template" \
 	--parameters "ParameterKey=KeyPairName,ParameterValue=DSTOQ_KP_1" \
+	             "ParameterKey=VaultAccessCIDR,ParameterValue=10.0.0.0/16" \
 	--timeout-in-minutes 30 \
 	--capabilities CAPABILITY_IAM \
-	
+
+elif [ "$1" == "update" ]; then
+
+    aws cloudformation update-stack \
+        --stack-name "Vault-Staging" \
+	--template-url "http://quickstart-hashicorp-vault.s3.amazonaws.com/templates/quickstart-hashicorp-vault-master.template" \
+	--parameters "ParameterKey=KeyPairName,ParameterValue=DSTOQ_KP_1" \
+	             "ParameterKey=VaultAccessCIDR,ParameterValue=10.0.0.0/16" \
+	--capabilities CAPABILITY_IAM \
+
+fi
